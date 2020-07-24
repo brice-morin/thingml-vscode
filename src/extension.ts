bb7a546a000e492aa8fc366bbfb02ceacec1acbc
brice-morin <brice.morin1@gmail.com>
2020-07-24 16:01:03 +0200

    small changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,13 @@
 'use strict';
 
-import * as net from 'net';
+import {connect} from 'net';
 
 import {Trace} from 'vscode-jsonrpc';
-import { window, workspace, commands, ExtensionContext, Uri, Terminal } from 'vscode';
-import { LanguageClient, LanguageClientOptions, StreamInfo, Position as LSPosition, Location as LSLocation, ServerOptions } from 'vscode-languageclient';
+import { window, workspace, commands, ExtensionContext, Terminal, ProgressLocation } from 'vscode';
+import { LanguageClient, LanguageClientOptions, StreamInfo } from 'vscode-languageclient';
 
-import { exec } from 'child_process'
-import os = require('os')
+import fs = require('fs')
 import path = require('path')
-import { resolve } from 'path';
 
 function startThingML(context: ExtensionContext) {
     return new Promise(resolve => {
@@ -20,11 +18,40 @@ function startThingML(context: ExtensionContext) {
         terminal.sendText('java -jar ' + context.asAbsolutePath(path.join('server', 'thingml.ide-2.0.0-SNAPSHOT-ls.jar')).replace(/\\/g,'\\\\'))    
         setTimeout(() => {
           resolve('OK');
-        }, 2000);
+        }, 1000);
     });
 }
 
-function generateCode(terminal: Terminal, context: ExtensionContext, compiler: String, source: String, output: String) {    
+function generateCodeWithProgress(context: ExtensionContext, terminal: Terminal, compiler: string, source: string, output: string) {
+    window.withProgress({
+        location: ProgressLocation.Notification,
+        title: 'Compiling ' + source + ' to ' + compiler + ' in folder ' + output,
+        cancellable: false
+    }, (progress, _token) => {        
+		const p = new Promise(resolve => {
+            let count = 0
+            progress.report({increment: count})
+            generateCode(terminal, context, compiler, source, output)  
+            let timer: NodeJS.Timeout 
+            timer = setInterval(()=>{
+                count += 2
+                if (count <= 100) {
+                    progress.report({increment: count})
+                }
+            },200);
+            
+            fs.watch(output, () => {
+                progress.report({increment: 100})
+                clearInterval(timer)
+                resolve()
+            })            
+        });        
+        return p
+    })
+}
+
+function generateCode(terminal: Terminal, context: ExtensionContext, compiler: string, source: string, output: string) { 
+    console.log('Compiling ' + source + ' to ' + compiler + ' in folder ' + output)   
     terminal.sendText('java -cp ' + context.asAbsolutePath(path.join('server', 'thingml.ide-2.0.0-SNAPSHOT-ls.jar ')).replace(/\\/g,'\\\\'), false)
     terminal.sendText('org.thingml.compilers.commandline.Main --compiler ' + compiler + ' --source ' + source.replace(/\\/g,'\\\\') + ' --output ' + output.replace(/\\/g,'\\\\'))    
     //terminal.dispose()
@@ -33,16 +60,15 @@ function generateCode(terminal: Terminal, context: ExtensionContext, compiler: S
 export async function activate(context: ExtensionContext) {
     const serverPort = 5008
 
-    const result = await startThingML(context)    
-    console.log(result);
-
+    await startThingML(context)    
+    
     // The server is a started as a separate app and listens on port 5008
     let connectionInfo = {
         port: serverPort
     };
     let serverOptions = () => {
         // Connect to language server via socket
-        let socket = net.connect(connectionInfo);
+        let socket = connect(connectionInfo);
         let result: StreamInfo = {
             writer: socket,
             reader: socket
@@ -74,45 +100,17 @@ export async function activate(context: ExtensionContext) {
         name: `ThingML Compiler`
     } as any);    
 
-    let compileJava = commands.registerTextEditorCommand('thingml.compile.java', (texteditor, edit, args) => {
-        console.log('Compiling ' + texteditor.document.uri.fsPath.toString() + ' to Java...')
-        generateCode(terminal, context, 'java', texteditor.document.uri.fsPath.toString(), path.join(path.dirname(texteditor.document.uri.fsPath), 'thingml-gen', 'java'))
-    })
-    context.subscriptions.push(compileJava)
 
-    let compilePosix = commands.registerTextEditorCommand('thingml.compile.posix', (texteditor, edit, args) => {
-        console.log('Compiling ' + texteditor.document.uri.fsPath.toString() + ' to POSIX/C')
-        generateCode(terminal, context, 'posix', texteditor.document.uri.fsPath.toString(), path.join(path.dirname(texteditor.document.uri.fsPath), 'thingml-gen', 'posix'))
-    })
-    context.subscriptions.push(compilePosix)
+    const compilers = ['java', 'posix', 'arduino', 'go', 'nodejs', 'browser', 'uml']
+    
+    compilers.forEach(compiler => {
+        //console.log('registering command thingml.compile.' + compiler)
+        let compile = commands.registerTextEditorCommand('thingml.compile.' + compiler, (texteditor, edit, args) => {
+            const source = texteditor.document.uri.fsPath.toString()
+            const output = path.join(path.dirname(texteditor.document.uri.fsPath), 'thingml-gen', compiler)
+            generateCodeWithProgress(context, terminal, compiler, source, output)
+        })
+        context.subscriptions.push(compile)
+    });
 
-    let compileArduino = commands.registerTextEditorCommand('thingml.compile.arduino', (texteditor, edit, args) => {
-        console.log('Compiling ' + texteditor.document.uri.fsPath.toString() + ' to Arduino/C')
-        generateCode(terminal, context, 'arduino', texteditor.document.uri.fsPath.toString(), path.join(path.dirname(texteditor.document.uri.fsPath), 'thingml-gen', 'arduino'))
-    })
-    context.subscriptions.push(compileArduino)
-
-    let compileGo = commands.registerTextEditorCommand('thingml.compile.go', (texteditor, edit, args) => {
-        console.log('Compiling ' + texteditor.document.uri.fsPath.toString() + ' to Go')
-        generateCode(terminal, context, 'go', texteditor.document.uri.fsPath.toString(), path.join(path.dirname(texteditor.document.uri.fsPath), 'thingml-gen', 'go'))
-    })
-    context.subscriptions.push(compileGo)
-
-    let compileNodeJS = commands.registerTextEditorCommand('thingml.compile.nodejs', (texteditor, edit, args) => {
-        console.log('Compiling ' + texteditor.document.uri.fsPath.toString() + ' to NodeJS')
-        generateCode(terminal, context, 'nodejs', texteditor.document.uri.fsPath.toString(), path.join(path.dirname(texteditor.document.uri.fsPath), 'thingml-gen', 'nodejs'))
-    })
-    context.subscriptions.push(compileNodeJS)
-
-    let compileBrowser = commands.registerTextEditorCommand('thingml.compile.browser', (texteditor, edit, args) => {
-        console.log('Compiling ' + texteditor.document.uri.fsPath.toString() + ' to JS for browser')
-        generateCode(terminal, context, 'browser', texteditor.document.uri.fsPath.toString(), path.join(path.dirname(texteditor.document.uri.fsPath), 'thingml-gen', 'browser'))
-    })
-    context.subscriptions.push(compileBrowser)
-
-    let compileUML = commands.registerTextEditorCommand('thingml.compile.uml', (texteditor, edit, args) => {
-        console.log('Compiling ' + texteditor.document.uri.fsPath.toString() + ' to UML')
-        generateCode(terminal, context, 'uml', texteditor.document.uri.fsPath.toString(), path.join(path.dirname(texteditor.document.uri.fsPath), 'thingml-gen', 'uml'))
-    })
-    context.subscriptions.push(compileUML)
 }
